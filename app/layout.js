f3eb000d4505494066437ddc04657ba5459c8daf
jpnel05 <deguzmanjohnpatrick02@gmail.com>
2026-03-11 17:37:06 -0400

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,9 @@
+import './globals.css'
+export const metadata = { title: "Rapport de Quart – McDonald's Alma" }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  )
+}
